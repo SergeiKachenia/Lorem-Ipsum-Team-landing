3efc8c9a1ddd484251affba06329c2e906bc917c
Haping61 <tg.trippl3a@gmail.com>
2022-12-10 17:42:18 +0700

    feat: добавлен адаптив для компонента Header * добавлен адаптив для компонента Header * добавлено бургер-меню и анимация для него * исправлены стили и верстка для компонента Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,30 @@
 import cn from 'classnames';
 
-import React from 'react';
+import BurgerMenu from 'components/BurgerMenu/BurgerMenu';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import styles from './Header.module.scss';
 
 const Header: React.FC = () => {
+  const [isOpen, setOpen] = useState(false);
   return (
     <header className={styles.header}>
-      <div className={styles.container}>
-        <nav>
-          <ul className={styles.list}>
-            <li>
-              <NavLink className={({ isActive }) => cn(styles.link, { [styles.link_active]: isActive })} to='/'>
-                Первая страница
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                className={({ isActive }) => cn(styles.link, { [styles.link_active]: isActive })}
-                to={'/projects'}
-              >
-                Вторая страница
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <nav className={styles.container}>
+        <ul className={cn(styles.menu, { [styles.menu_active]: isOpen })}>
+          <li>
+            <NavLink className={({ isActive }) => cn(styles.link, { [styles.link_active]: isActive })} to='/'>
+              Первая страница
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => cn(styles.link, { [styles.link_active]: isActive })} to={'/projects'}>
+              Вторая страница
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <BurgerMenu onClick={() => setOpen(!isOpen)}></BurgerMenu>
     </header>
   );
 };
