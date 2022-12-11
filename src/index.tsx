2f1c90e9d37378c7fb5c1a8d896a3f25e3f6ebbb
AnimeLore <animelorenoreply@gmail.com>
2022-12-11 15:37:08 +0700

    fix: исправлено подключение шрифтов

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import './fonts/YS/stylesheet.css';
 import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
