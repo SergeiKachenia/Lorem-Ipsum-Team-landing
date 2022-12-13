5013fc2d19f7de906287a87135bacb3937bffeb5
Danxay <dan.dryagin@gmail.com>
2022-12-14 02:04:58 +0600

    fix: Исправления
    * Исправлены стили
    * Добавлен кастомный чекбокс
    * Исправлена валидация

--- src/components/FeedbackForm/FeedbackForm.tsx
+++ src/components/FeedbackForm/FeedbackForm.tsx
@@ -18,11 +18,11 @@ const FeedbackForm: React.FC<ICloseForm> = ({ closeForm }) => {
       name: '',
       email: '',
       message: '',
-      dataTreat: '',
+      dataTreat: [],
     },
     validationSchema: Yup.object({
-      name: Yup.string().max(30, 'Слишком длинное имя'),
-      email: Yup.string().email('Некорректный email'),
+      name: Yup.string().required('Введите ваше имя').max(30, 'Слишком длинное имя'),
+      email: Yup.string().required('Введите ваш email').email('Некорректный email'),
       message: Yup.string().required('Сообщение не может быть пустым'),
       dataTreat: Yup.array().min(1, 'Даю согласие на обработку персональных данных'),
     }),
@@ -30,7 +30,7 @@ const FeedbackForm: React.FC<ICloseForm> = ({ closeForm }) => {
       console.log(values);
     },
   });
-  const condName = (key: 'name' | 'message' | 'email' | 'dataTreat'): boolean => {
+  const condName = (key: keyof typeof formik.touched | keyof typeof formik.errors): boolean => {
     if (formik.touched[key] !== undefined && formik.errors[key] !== undefined) {
       return false;
     }
@@ -38,44 +38,43 @@ const FeedbackForm: React.FC<ICloseForm> = ({ closeForm }) => {
   };
   const isFormFull =
     Object.values(formik.values).filter((val) => val === '').length === 0 && Object.entries(formik.errors).length === 0;
-  console.log(isFormFull);
+
   return (
-    <div className={cn(styles.wrapper)}>
-      <div className={cn(styles.background)}></div>
-      <form className={cn(styles.form)} onSubmit={formik.handleSubmit}>
-        <MdClose className={cn(styles.closeBtn)} onClick={closeForm}></MdClose>
-        <h2 className={cn(styles.title)}>Форма для связи с нами</h2>
-        <div className={cn(styles.field)}>
+    <div className={styles.wrapper}>
+      <div className={styles.background}></div>
+      <form className={styles.form} onSubmit={formik.handleSubmit}>
+        <MdClose className={styles.closeBtn} onClick={closeForm}></MdClose>
+        <h2 className={styles.title}>Форма для связи с нами</h2>
+        <div className={styles.field}>
           <label className={cn(styles.label, { [styles.validError]: !condName('name') })}>
             {condName('name') ? 'Как вас зовут' : formik.errors.name}
           </label>
           <input
             name='name'
             type='text'
-            className={cn(styles.input)}
+            className={styles.input}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             value={formik.values.name}
           />
         </div>
-        <div className={cn(styles.field)}>
+        <div className={styles.field}>
           <label className={cn(styles.label, { [styles.validError]: !condName('email') })}>
             {condName('email') ? 'Ваш email' : formik.errors.email}
           </label>
           <input
             name='email'
             type='email'
-            className={cn(styles.input)}
+            className={styles.input}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             value={formik.values.email}
           />
         </div>
-        <div className={cn(styles.field)}>
+        <div className={styles.field}>
           <label
             className={cn(styles.label, {
               [styles.validError]: !condName('message'),
-              [styles.width_128]: !condName('message'),
             })}
           >
             {condName('message') ? 'Ваше сообщение' : formik.errors.message}
@@ -83,19 +82,19 @@ const FeedbackForm: React.FC<ICloseForm> = ({ closeForm }) => {
           <textarea
             name='message'
             rows={5}
-            className={cn(styles.input)}
+            className={styles.input}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             value={formik.values.message}
           />
         </div>
-        <div className={cn(styles.data_check)}>
-          <input name='dataTreat' type='checkbox' className={cn(styles.checkbox)} onChange={formik.handleChange} />
-          <label className={cn(styles.data_check__label)}>
+        <div className={styles.data_check}>
+          <input name='dataTreat' type='checkbox' className={styles.checkbox} onChange={formik.handleChange} />
+          <label className={styles.data_check__label}>
             {condName('dataTreat') ? 'Даю согласие на обработку персональных данных' : formik.errors.dataTreat}
           </label>
         </div>
-        <button className={cn(styles.send_btn)} type='submit' disabled={!isFormFull}>
+        <button className={styles.send_btn} type='submit' disabled={!isFormFull}>
           Отправить
         </button>
       </form>
