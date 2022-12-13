f70331ebdbe57a58bb9f97c806ef27248cd5e54e
AnimeLore <animelorenoreply@gmail.com>
2022-12-13 14:27:42 +0700

    feat: временно убран компонент header из App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import { ContactInfo } from 'components/ContactInfo/ContactInfo';
-import Header from 'components/Header/Header';
 import React from 'react';
-<<<<<<<<< Temporary merge branch 1
 import { BrowserRouter } from 'react-router-dom';
 
+// import Header from './components/Header/Header';
 import { OurTeam } from './components/OurTeam/OurTeam';
 import { mockTeamMembers } from './constants/mock';
 export const App: React.FC = (): JSX.Element => {
   return (
     <BrowserRouter>
-      <OurTeam teamList={mockTeamMembers} />
-    </BrowserRouter>
-=========
-
-export const App: React.FC = (): JSX.Element => {
-  return (
-    <>
-      <Header />
+      {/* <Header /> */}
       <main>
+        <OurTeam teamList={mockTeamMembers} />
         <ContactInfo />
       </main>
-    </>
->>>>>>>>> Temporary merge branch 2
+    </BrowserRouter>
   );
 };
