1f3fecfbbc67c436ac66bd1d1a4c5d58ead1c38d
Danxay <dan.dryagin@gmail.com>
2022-12-13 17:38:10 +0600

    feat: Добавлен показатель страниц слайдера
    * Добавлен показатель страниц слайдера и стили к нему

--- src/components/OurTeam/OurTeam.tsx
+++ src/components/OurTeam/OurTeam.tsx
@@ -1,8 +1,11 @@
+import cn from 'classnames';
+
 import React, { useMemo, useRef } from 'react';
 import AliceCarousel from 'react-alice-carousel';
 
 import 'react-alice-carousel/lib/scss/alice-carousel.scss';
-import styles from './styles.module.scss';
+import styles from './OurTeam.module.scss';
+import './AliceCarousel.scss';
 
 import { IOurTeamProps } from '../../types/IOurTeamProps';
 import { ArrowButton } from '../ArrowButton/ArrowButton';
@@ -36,28 +39,34 @@ export const OurTeam: React.FC<IOurTeamProps> = ({ teamList }) => {
   const prevSlideHandler = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
     carousel?.current?.slidePrev(e);
   };
+  const renderDotsItem = ({ isActive }: { isActive: boolean }): React.ReactNode => {
+    return <div className={cn([styles.currentPageItem, { [styles.active]: isActive }])} />;
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.headerContainer}>
-        <span className={styles.headerText}>Наша команда</span>
-        <div className={styles.buttons}>
-          <button className={styles.button} onClick={prevSlideHandler}>
-            <ArrowButton isLeft={true} />
-          </button>
-          <button className={styles.button} onClick={nextSlideHandler}>
-            <ArrowButton isLeft={false} />
-          </button>
+    <section className={styles.ourTeam}>
+      <div className={styles.container}>
+        <div className={styles.headerContainer}>
+          <span className={styles.headerText}>Наша команда</span>
+          <div className={styles.buttons}>
+            <button className={styles.button} onClick={prevSlideHandler}>
+              <ArrowButton isLeft={true} />
+            </button>
+            <button className={styles.button} onClick={nextSlideHandler}>
+              <ArrowButton isLeft={false} />
+            </button>
+          </div>
         </div>
+        <AliceCarousel
+          disableButtonsControls
+          responsive={responsive}
+          items={teamListMapped}
+          ref={carousel}
+          renderDotsItem={renderDotsItem}
+          infinite={true}
+          mouseTracking
+        />
       </div>
-      <AliceCarousel
-        disableDotsControls
-        disableButtonsControls
-        responsive={responsive}
-        items={teamListMapped}
-        ref={carousel}
-        infinite={true}
-        mouseTracking
-      />
-    </div>
+    </section>
   );
 };
