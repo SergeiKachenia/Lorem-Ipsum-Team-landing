718b8ec71bb02df4feda36aaa5d603c6182872b3
Danxay <dan.dryagin@gmail.com>
2022-12-16 01:09:16 +0600

    feat: добавлен адаптив для компонента FeedbackForm

--- src/components/FeedbackForm/FeedbackForm.tsx
+++ src/components/FeedbackForm/FeedbackForm.tsx
@@ -12,7 +12,8 @@ import styles from './FeedbackForm.module.scss';
 
 const FeedbackForm: React.FC<ICloseForm> = ({ closeForm }) => {
   const [buttonPressed, setPressed] = useState(false);
-  const [resultMesage, setMessage] = useState('');
+  const [resultMessage, setMessage] = useState('');
+
   const formik = useFormik({
     initialValues: {
       name: '',
@@ -62,84 +63,86 @@ const FeedbackForm: React.FC<ICloseForm> = ({ closeForm }) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.background} onClick={closeForm}></div>
-      <form className={styles.form} onSubmit={formik.handleSubmit}>
-        <MdClose className={styles.closeBtn} onClick={closeForm}></MdClose>
-        <h2 className={styles.title}>Форма для связи с нами</h2>
-        <div className={styles.field}>
-          <label className={cn(styles.label)}>Как вас зовут</label>
-          <input
-            name='name'
-            type='text'
-            className={styles.input}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.name}
-          />
-        </div>
-        <span
-          className={cn(styles.errorSpan, {
-            [styles.validError]: !hasError('name'),
-          })}
-        >
-          {hasError('name') ? '' : formik.errors.name}
-        </span>
-        <div className={styles.field}>
-          <label className={cn(styles.label)}>Ваш email</label>
-          <input
-            name='email'
-            type='email'
-            className={styles.input}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.email}
-          />
-        </div>
-        <span
-          className={cn(styles.errorSpan, {
-            [styles.validError]: !hasError('email'),
-          })}
-        >
-          {hasError('email') ? '' : formik.errors.email}
-        </span>
-        <div className={styles.field}>
-          <label className={cn(styles.label)}>Ваше сообщение</label>
-          <textarea
-            name='message'
-            rows={5}
-            className={cn(styles.input, styles.textarea)}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.message}
-          />
-        </div>
-        <span
-          className={cn(styles.errorSpan, {
-            [styles.validError]: !hasError('message'),
-          })}
-        >
-          {hasError('message') ? '' : formik.errors.message}
-        </span>
-        <div className={styles.data_check}>
-          <input
-            id='dataTreat'
-            name='dataTreat'
-            type='checkbox'
-            className={styles.checkbox}
-            onChange={formik.handleChange}
-          />
-          <label htmlFor='dataTreat' className={styles.data_check__label}>
-            {hasError('dataTreat') ? 'Даю согласие на обработку персональных данных' : formik.errors.dataTreat}
-          </label>
-        </div>
-        <button
-          className={styles.send_btn}
-          type='submit'
-          disabled={!(formik.isValid && formik.dirty) || formik.isSubmitting}
-        >
-          Отправить
-        </button>
-      </form>
-      <span className={cn(styles.hiddenMessage, { [styles.resultMesage]: buttonPressed })}>{resultMesage}</span>
+      <div className={styles.container}>
+        <form className={styles.form} onSubmit={formik.handleSubmit}>
+          <MdClose className={styles.closeBtn} onClick={closeForm}></MdClose>
+          <h2 className={styles.title}>Форма для связи с нами</h2>
+          <div className={styles.field}>
+            <label className={cn(styles.label)}>Как вас зовут</label>
+            <input
+              name='name'
+              type='text'
+              className={styles.input}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.name}
+            />
+          </div>
+          <span
+            className={cn(styles.errorSpan, {
+              [styles.validError]: !hasError('name'),
+            })}
+          >
+            {hasError('name') ? '' : formik.errors.name}
+          </span>
+          <div className={styles.field}>
+            <label className={cn(styles.label)}>Ваш email</label>
+            <input
+              name='email'
+              type='email'
+              className={styles.input}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+            />
+          </div>
+          <span
+            className={cn(styles.errorSpan, {
+              [styles.validError]: !hasError('email'),
+            })}
+          >
+            {hasError('email') ? '' : formik.errors.email}
+          </span>
+          <div className={styles.field}>
+            <label className={cn(styles.label)}>Ваше сообщение</label>
+            <textarea
+              name='message'
+              rows={5}
+              className={cn(styles.input, styles.textarea)}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.message}
+            />
+          </div>
+          <span
+            className={cn(styles.errorSpan, {
+              [styles.validError]: !hasError('message'),
+            })}
+          >
+            {hasError('message') ? '' : formik.errors.message}
+          </span>
+          <div className={styles.data_check}>
+            <input
+              id='dataTreat'
+              name='dataTreat'
+              type='checkbox'
+              className={styles.checkbox}
+              onChange={formik.handleChange}
+            />
+            <label htmlFor='dataTreat' className={styles.data_check__label}>
+              {hasError('dataTreat') ? 'Даю согласие на обработку персональных данных' : formik.errors.dataTreat}
+            </label>
+          </div>
+          <button
+            className={styles.send_btn}
+            type='submit'
+            disabled={!(formik.isValid && formik.dirty) || formik.isSubmitting}
+          >
+            Отправить
+          </button>
+        </form>
+      </div>
+      <span className={cn(styles.hiddenMessage, { [styles.resultMessage]: buttonPressed })}>{resultMessage}</span>
     </div>
   );
 };
