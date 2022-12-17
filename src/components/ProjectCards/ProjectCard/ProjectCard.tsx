c848ab4f77ea38fd4ea311344949ae31daad19ae
Danxay <dan.dryagin@gmail.com>
2022-12-17 17:02:37 +0600

    feat: наработки карточки проекта

---
+++ src/components/ProjectCards/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './ProjectCards.module.scss';
+
+export const ProjectCards: React.FC = () => {
+  return (
+    <div>
+      <div>123</div>
+    </div>
+  );
+};
