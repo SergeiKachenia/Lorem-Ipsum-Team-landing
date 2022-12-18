1c56bf7c5ee8755dcfb5283950fd9c9c30556af5
SergeiKachenia <serezha100500@mail.ru>
2022-12-18 17:28:11 +0700

    merge fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-
+import Footer from 'components/Footer/Footer';
+import Header from 'components/Header/Header';
+import AboutUsPage from 'pages/AboutUsPage/AboutUsPage';
+import ProjectsPage from 'pages/ProjectsPage/ProjectsPage';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { store } from './store';
 
-// Components and pages
-import Footer from 'components/Footer/Footer';
-import Header from 'components/Header/Header';
-import AboutUsPage from 'pages/AboutUsPage';
-import ProjectsPage from './pages/ProjectsPage/ProjectsPage';
+import { store } from './store';
 
 export const App: React.FC = (): JSX.Element => {
   return (
-      <Provider store={store}>
-    <BrowserRouter>
-      <Header />
-      <main>
-        <Routes>
-          <Route index element={<AboutUsPage />} />
-          <Route path={'/projects'} element={<ProjectsPage />} />
-        </Routes>
-      </main>
-      <Footer />
-    </BrowserRouter>
-        </Provider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <main>
+          <Routes>
+            <Route index element={<AboutUsPage />} />
+            <Route path={'/projects'} element={<ProjectsPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </BrowserRouter>
+    </Provider>
+  );
+};
