41ae62612ceb4a6acf544254a31b44f63789c0f2
Danxay <dan.dryagin@gmail.com>
2022-12-20 13:30:04 +0600

    fix: Удален объект shortProjectsInfo
    * В loadProjects добалено преобразование IProjectsInfo в IShortProjectsInfo

--- src/pages/ProjectsPage/ProjectsPage.tsx
+++ src/pages/ProjectsPage/ProjectsPage.tsx
@@ -16,22 +16,21 @@ const ProjectsPage: React.FC = () => {
         <h1 className={styles.title}>
           <TextLocales locale={(l) => locales.OurProjects[l]} />
         </h1>
-        <h1 className={styles.title}>Наши проекты</h1>
         <ProjectCards />
       </div>
-      <ProjectPopup
-        author={projectsInfo[0].author}
-        date={projectsInfo[0].date}
-        target={projectsInfo[0].target}
-        stack={projectsInfo[0].stack}
-        description={projectsInfo[0].description}
-        imagesUrls={projectsInfo[0].imagesUrls}
-        title={projectsInfo[0].title}
-        link={projectsInfo[0].link}
-        closePopup={() => {
-          alert('insert handler there');
-        }}
-      />
+      {/* <ProjectPopup */}
+      {/*  author={projectsInfo[0].author} */}
+      {/*  date={projectsInfo[0].date} */}
+      {/*  target={projectsInfo[0].target} */}
+      {/*  stack={projectsInfo[0].stack} */}
+      {/*  description={projectsInfo[0].description} */}
+      {/*  imagesUrls={projectsInfo[0].imagesUrls} */}
+      {/*  title={projectsInfo[0].title} */}
+      {/*  link={projectsInfo[0].link} */}
+      {/*  closePopup={() => { */}
+      {/*    alert('insert handler there'); */}
+      {/*  }} */}
+      {/* /> */}
     </>
   );
 };
