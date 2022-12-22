1323a70acd40c7c38999dbffe6a66e1fc6960c70
AnimeLore <animelorenoreply@gmail.com>
2022-12-22 18:02:48 +0700

    fix: удалён лишний пробел

--- src/constants/modulesLocales/CallToAction.ts
+++ src/constants/modulesLocales/CallToAction.ts
@@ -4,7 +4,7 @@ export const locales = {
     en: 'Development of various web applications.',
   },
   Description: {
-    ru: 'Сайты, приложения и многое другое. Под ключ, от профессионалов с огромным опытом работы. ',
+    ru: 'Сайты, приложения и многое другое. Под ключ, от профессионалов с огромным опытом работы.',
     en: 'Websites, apps, and more. On turn-key basis, from professionals with extensive experience.',
   },
   Order: {
